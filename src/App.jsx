ff4ac3a513d7d741afdd3de090c3c77822b7c1c2
iagosoave <iago.soave@gmail.com>
2025-10-07 16:48:21 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import HeroSection from './components/Hero';
 import MasterclassSection from './components/MasterclassSection';
 import BeneficiarSection from './components/BeneficiarSection';
 import CTASection  from './components/CTASection';
-import Aboutsection  from './components/aboutsection';
+import Aboutsection  from './components/Aboutsection';
 
 function App() {
   return (
